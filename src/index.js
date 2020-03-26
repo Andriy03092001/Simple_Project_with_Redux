a50f0d02c4159f718558c09bac3bf48848b24708
Andriy03092001 <cuanid236316@gmail.com>
2020-03-26 17:22:52 +0200

    upload project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component, Fragment } from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import "./index.js"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const initialState = 0;
+
+const reducer = (state = initialState, action) => {
+  // console.log("Init state: ", state, "\n action: ", action);
+  switch (action.type) {
+    case "PLUS": {
+      return state + 1
+    }
+    case "MINUS": {
+      return state - 1;
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+const store = createStore(reducer);
+
+class App extends Component {
+
+  plusCounter = () => {
+    store.dispatch({ type: "PLUS" });
+  }
+
+  minusCounter = () => {
+    store.dispatch({ type: "MINUS" });
+  }
+
+  render() {
+    return (
+      < Fragment >
+        <h1 id="myCounter">0</h1>
+        <button className="btn btn-info " onClick={this.plusCounter}>+</button>
+        <button className="btn btn-info " onClick={this.minusCounter}>-</button>
+      </Fragment >
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+store.subscribe(() => { //Обробка будь якої зміни в сторі
+  // console.log(store.getState());
+  document.getElementById("myCounter").innerHTML = store.getState();
+})
+
+
+
+// store.dispatch({ type: "PLUS" }); // по факту визиваємо екшен 
+// store.dispatch({ type: "PLUS" });
+// store.dispatch({ type: "PLUS" });
+// store.dispatch({ type: "PLUS" });
+// store.dispatch({ type: "PLUS" });
+
+// // console.log(store.getState());
+
+// store.dispatch({ type: "MINUS" }); // по факту визиваємо екшен 
+// console.log(store.getState());
+
+
+
